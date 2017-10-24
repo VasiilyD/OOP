d4d4326010d2839a14585a9d20060ec605f23875
VasiilyD <denysenko1@gmail.com>
2017-10-24 11:39:17 +0300

    first111 commit

--- HomeWork1a/src/vrd/com/Cat.java
+++ HomeWork1a/src/vrd/com/Cat.java
@@ -3,81 +3,67 @@ package vrd.com;
 import java.awt.*;
 
 public class Cat {
+	// Cfffff
 
-	 private String name;
-	    private int age;
-	    private String breed;
-	    private Color color;
-	    private boolean sleep;
+	private String name;
+	private int age;
+	private String breed;
+	private Color color;
+	private boolean sleep;
 
-	    public Cat(String name, int age, String breed, Color color) {
+	public Cat(String name, int age, String breed, Color color) {
 
-	        this.name = name;
-	        this.age = age;
-	        this.breed = breed;
-	        this.color = color;
-	        this.sleep = false;
-	    }
+		this.name = name;
+		this.age = age;
+		this.breed = breed;
+		this.color = color;
+		this.sleep = false;
+	}
 
-	    	    
-		public String getName() {
-			return name;
-		}
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 
-		public void setName(String name) {
-			this.name = name;
-		}
+	public int getAge() {
+		return age;
+	}
 
+	public void setAge(int age) {
+		this.age = age;
+	}
 
-		public int getAge() {
-			return age;
-		}
+	public String getBreed() {
+		return breed;
+	}
 
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
 
-		public void setAge(int age) {
-			this.age = age;
-		}
+	public Color getColor() {
+		return color;
+	}
 
+	public void setColor(Color color) {
+		this.color = color;
+	}
 
-		public String getBreed() {
-			return breed;
-		}
+	public boolean isSleep() {
+		return sleep;
+	}
 
+	public void setSleep(boolean sleep) {
+		this.sleep = sleep;
+	}
 
-		public void setBreed(String breed) {
-			this.breed = breed;
-		}
+	@Override
+	public String toString() {
+		return "Cat [name=" + name + ", age=" + age + ", breed=" + breed + ", color=" + color + ", sleep=" + sleep
+				+ "]";
+	}
 
-
-		public Color getColor() {
-			return color;
-		}
-
-
-		public void setColor(Color color) {
-			this.color = color;
-		}
-
-
-		public boolean isSleep() {
-			return sleep;
-		}
-
-
-		public void setSleep(boolean sleep) {
-			this.sleep = sleep;
-		}
-
-
-		@Override
-		public String toString() {
-			return "Cat [name=" + name + ", age=" + age + ", breed=" + breed + ", color=" + color + ", sleep=" + sleep
-					+ "]";
-		}
-	    
-	    
-	    
-
-	
 }
